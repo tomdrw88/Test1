401d0b63af65658701168fa49951d371194b4bd0
Tomasz Drwięga <tomdrw88@o2.pl>
2018-11-26 21:05:48 +0100

    Test1 - first commit

---
+++ Test1/src/main/java/com/example/test1/Test1.java
@@ -0,0 +1,30 @@
+/*
+ *  Company: RS
+ *  Project: Test1
+ *  Created: 26 lis 2018  20:18:18
+ *  Author:  RS
+ */
+package com.example.test1;
+
+/**
+ *<p></p><br>26 lis 2018
+ * @author RS
+ *
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		run1();
+
+	}
+	
+	
+	
+	private static void run1() {
+		System.out.println("Run 1");
+	}
+
+}
